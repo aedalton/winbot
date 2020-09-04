40f27a5f9506c09cc05a9d5f3980242a6986dbe4
Alexandra Dalton <a.e.dalton.16@gmail.com>
2020-09-04 13:51:47 -0400

    [update]: msg_generator: SLACK client change

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -3,13 +3,16 @@ import tempfile
 
 import pytest
 
-from winbot import AppFactory
+from winbot.app import AppFactory
 
 
 @pytest.fixture
 def client_fixture():
-    with winbot.AppFactory.create() as client:
+    with AppFactory.create() as client:
         with client.app_context():
+            response = SLACK_CONN.conversations_members(channel=SLACK_CHANNEL_ID)
+            user_ids = response["members"]
+            print(f"user_ids: {user_ids}")
             import pdb; pdb.set_trace()
         yield client
 
